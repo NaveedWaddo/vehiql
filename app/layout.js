9a1978c594a62d2ecfac5ed3229514cfec4b61c8
Naveed Waddo <naveedwaddo1123@gmail.com>
2025-04-27 19:35:54 +0530

    Custome Login/Signup page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,20 +12,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <Header />
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
 
-        <main className="min-h-screen">{children}</main>
+          <main className="min-h-screen">{children}</main>
 
-        <footer className="bg-blue-50 py-12">
-          <div>
-            <p className="container mx-auto px-4 text-center text-gray-600">
-              Made with 💗 Naveed Waddo
-            </p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          <footer className="bg-blue-50 py-12">
+            <div>
+              <p className="container mx-auto px-4 text-center text-gray-600">
+                Made with 💗 Naveed Waddo
+              </p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
