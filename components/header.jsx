a7244a2e806755d30dc2f512bae99500a39ad6bf
Naveed Waddo <naveedwaddo1123@gmail.com>
2025-04-27 20:00:23 +0530

    custome SignIn/SignUp page complete

--- components/header.jsx
+++ components/header.jsx
@@ -1,24 +1,5 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-import React from "react";
-
 const Header = () => {
-  return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
-  );
+  return <div>Header</div>;
 };
 
 export default Header;
